cb797a174381a9293e09f05dbf06d9ccccb30171
takerumimata <fossil1908@gmail.com>
2020-08-26 22:23:34 +0900

    fix error: no error occur

--- chapter1/step07.py
+++ chapter1/step07.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class Variable:
     def __init__(self, data):
         self.data = data
@@ -22,13 +23,14 @@ class Function:
     def forward(self, x):
         raise NotImplementedError()
 
-    @staticmethod
+    # @staticmethod
     def backward(self, gy):
         raise NotImplementedError()
 
 class Square(Function):
     def forward(self, x):
-        return x ** 2
+        y = x ** 2
+        return y
 
     def backward(self, gy):
         x = self.input.data
@@ -37,7 +39,8 @@ class Square(Function):
 
 class Exp(Function):
     def forward(self, x):
-        return np.exp(x)
+        y = np.exp(x)
+        return y
 
     def backward(self, gy):
         x = self.input.data
@@ -45,26 +48,24 @@ class Exp(Function):
         return gx
 
 def numerical_diff(f, x, eps=1e-4):
-    x0 = Variable(x.data - eps)
-    x1 = Variable(x.data + eps)
+    x0 = (x.data - eps)
+    x1 = (x.data + eps)
     y0 = f(x0)
     y1 = f(x1)
     return (y1.data - y0.data) / (2*eps)
 
-# 単純な微分の確認
-f = Square()
-x = Variable(np.array(2.0))
-dy = numerical_diff(f, x)
-
-print(dy)
-
-# 合成関数の微分の確認
-def f(x):
-    A = Square()
-    B = Exp()
-    C = Square()
-    return C(B(A(x)))
+A = Square()
+B = Exp()
+C = Square()
 
 x = Variable(np.array(0.5))
-dy = numerical_diff(f, x)
-print(dy)
+a = A(x)
+b = B(a)
+y = C(b)
+
+assert y.creator == C
+assert y.creator.input == b
+assert y.creator.input.creator == B
+assert y.creator.input.creator.input == a
+assert y.creator.input.creator.input.creator == A
+assert y.creator.input.creator.input.creator.input == x
