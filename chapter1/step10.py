9f8e943e9a4ba8fc310b57c9034e4a93b64f14ac
takerumimata <fossil1908@gmail.com>
2020-08-28 16:08:18 +0900

    add test

---
+++ chapter1/step10.py
@@ -0,0 +1,110 @@
+import numpy as np
+import unittest
+
+
+class Variable:
+    def __init__(self, data):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError('{} is not supported'.format(type(data)))
+        self.data = data
+        self.grad = None
+        self.creator = None
+    
+    def set_creator(self, func):
+        self.creator = func 
+    
+    def backward(self):
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+        funcs = [self.creator] 
+        while funcs:
+            f = funcs.pop() # 関数を取得
+            x, y = f.input, f.output # 関数の入出力を取得
+            x.grad = f.backward(y.grad) # backwardメソッドを呼ぶ
+            if x.creator is not None:
+                funcs.append(x.creator) # 1つ前の関数をリストに追加
+
+class Function:
+    def __call__(self, input):
+        x = input.data
+        y = self.forward(x)
+        output = Variable(as_array(y))
+        output.set_creator(self) # 出力変数に生みのおやを覚えさせる
+        self.input = input
+        self.output = output # 出力も覚える
+        return output
+
+    def forward(self, x):
+        raise NotImplementedError()
+
+    # @staticmethod
+    def backward(self, gy):
+        raise NotImplementedError()
+
+class Square(Function):
+    def forward(self, x):
+        y = x ** 2
+        return y
+
+    def backward(self, gy):
+        x = self.input.data
+        gx = 2 * x * gy
+        return gx
+
+class Exp(Function):
+    def forward(self, x):
+        y = np.exp(x)
+        return y
+
+    def backward(self, gy):
+        x = self.input.data
+        gx = np.exp(x) * gy
+        return gx
+
+class SquareTest(unittest.TestCase):
+    def test_forward(self):
+        x = Variable(np.array(2.0))
+        y = square(x)
+        expected = np.array(4.0)
+        self.assertEqual(y.data, expected)
+
+    def test_backward(self):
+        x = Variable(np.array(3.0))
+        y = square(x)
+        y.backward()
+        expected = np.array(6.0)
+        self.assertEqual(x.grad, expected)
+
+    def test_gradient_check(self):
+        x = Variavle(np.random.rand(1)) # ランダムな入力値を生成
+        y = square(x)
+        y.backward()
+        num_grad = numerical_diff(square, x)
+        flg = np.allclose(x.grad, num_grad)
+        self.assertTrue(flg)
+
+def numerical_diff(f, x, eps=1e-4):
+    x0 = (x.data - eps)
+    x1 = (x.data + eps)
+    y0 = f(x0)
+    y1 = f(x1)
+    return (y1.data - y0.data) / (2*eps)
+
+def square(x):
+    return Square()(x)  # squareクラスをわざわざ生成するのがめんどくさいので、pythonのメソッドとして定義する。
+
+def exp(x):
+    return Exp()(x)
+
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
+
+x = Variable(np.array(0.5))
+y = square(exp(square(x)))
+# 逆伝播
+y.backward()
+print(x.grad)
+
